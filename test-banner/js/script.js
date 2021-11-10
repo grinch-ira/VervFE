ffa79c75c0511cab395943942f80861cafd98dd2
Hryniuk Iryna <grinch.ira@gmail.com>
2021-11-10 15:01:03 +0300

    feat: add price and button

--- test-banner/js/script.js
+++ test-banner/js/script.js
@@ -1,1 +1,15 @@
-new Swiper('.text-slider');
+new Swiper('.text-slider',{
+         spaceBetween: 3,
+        slidePerGroup:1,
+        centeredSlides:true,
+        slidesPerView:1.1,
+        initialSlide:0,
+       loop:true, 
+        loopedSlides:3,
+        autoplay:{
+            delay:5000,
+            stopOnLastSlide:false,
+            disableOnInteraction:true
+        },
+        speed:1000,
+});
