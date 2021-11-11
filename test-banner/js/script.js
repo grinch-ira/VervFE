6fd80d280f60d7929cd235b15c8c9f08f1cd5ba6
Hryniuk Iryna <grinch.ira@gmail.com>
2021-11-11 16:21:30 +0300

    feat: update dark theme and add radiobutton

--- test-banner/js/script.js
+++ test-banner/js/script.js
@@ -12,6 +12,7 @@ new Swiper('.text-slider',{
             disableOnInteraction:true
         },
         speed:1000,
+
 });
 
 
@@ -19,6 +20,9 @@ new Swiper('.text-slider',{
 
 
 function getTimeOfDay(){
+    const swiperSlidePrev = document.querySelector('.swiper-slide-prev .text-slider__comments' )
+    const swiperSlide = document.querySelector('.swiper-slide-active .text-slider__comments ')
+    const swiperSlideNext = document.querySelector('.swiper-slide-next .text-slider__comments')
     const textSliderComments = document.querySelector('.text-slider__comments')
     const priceBlock1 = document.querySelector('.price__block1')
     const priceBlock2 = document.querySelector('.price__block2')
@@ -27,7 +31,7 @@ function getTimeOfDay(){
     const footerText = document.querySelector('.footer-text')
     const date = new Date();
     const hours = date.getHours();  
-     if(hours>=15 && hours<24){
+     if(hours>=18 && hours<24){
         document.body.style.backgroundColor = "black";
         priceBlock1.style.backgroundColor = '#373640';
         priceBlock2.style.backgroundColor = '#373640';
@@ -36,7 +40,32 @@ function getTimeOfDay(){
         footerText.style.color = '#7D7D80';
         textSliderComments.style.background = 'linear-gradient(225deg, #262526 0%, #19181A 100%)';
         textSliderComments.style.color = '#ffffff'
+        swiperSlide.style.background = 'linear-gradient(225deg, #262526 0%, #19181A 100%)'
+        swiperSlideNext.style.background = 'linear-gradient(225deg, #262526 0%, #19181A 100%)'
+        swiperSlidePrev.style.background = 'linear-gradient(225deg, #262526 0%, #19181A 100%)'
+        swiperSlide.style.color = '#ffffff'
+        swiperSlidePrev.style.color = '#ffffff'
+        swiperSlideNext.style.color = '#ffffff'
+    }
+    setTimeout(getTimeOfDay, 1000)
+}
+getTimeOfDay();
+
+function chooseLink(){
+    const radioBlock1 = document.getElementById('radio1')
+    const radioBlock2 = document.getElementById('radio2')
+    const radioBlock3 = document.getElementById('radio3')
+    if(radioBlock1.checked){
+         return 'https://www.google.com/search?q=1'
+    }
+    else if(radioBlock2.checked){
+        return 'https://www.google.com/search?q=2'
+    }
+   else {
+        return 'https://www.google.com/search?q=3'
     }
 
 }
-getTimeOfDay();
+
+console.log(chooseLink())
+
